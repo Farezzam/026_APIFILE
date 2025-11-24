48ef46cd6dd8c7ceff9647291f70774ce2d40007
ASUS <faris13abiyu@gmail.com>
2025-11-24 12:27:19 +0700

    edit file js (index, api, komikServices, komikController)

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectionbase = require('./config/db');
+const apiRoute = require('./routes/api');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', apiRoute);
+
+async function startServer() {
+    await connectionbase();
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
+}
+
+startServer();
---
+++ services/komikService.js
@@ -0,0 +1,62 @@
+async function createKomik(database, komikData, ) {
+    const {title, description, author, imageType, imageName, ImageData} = komikData;
+
+    if (!title || !description || !author) {
+        throw new Error('Title, description, dan author wajib diisi');
+    }
+
+    const newKomik = await database.Komik.create({
+        title,
+        description,
+        author,
+        imageType: imageType || null,
+        imageName: imageName || null,
+        ImageData: ImageData || null
+    });
+
+    return newKomik;
+}
+
+async function getAllKomiks(database) {
+    const komiks = await database.Komik.findAll();
+    return komik.map(k =>{
+        if (k.imageData) {
+            k.imageData = k.imageData.toString('base64');
+        }
+        return k;
+    });
+}
+
+async function getKomikById(database, id) {
+    const komik = await database.Komik.findByPk(id);
+    if (komik.ImageData) {
+        komik.imageData = komik.ImageData.toString('base64');
+    }
+    return komik;
+}
+
+async function updateKomik(database, id, komikData) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) {
+        throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    }
+    await komik.update(komikData);
+    return komik;
+}
+
+async function deleteKomik(database, id) {
+    const komik = await database.Komik.findByPk(id);
+    if (!komik) {
+        throw new Error(`Komik dengan ID ${id} tidak ditemukan`);
+    }
+    await komik.destroy();
+    return {message: `Komik dengan ID ${id} telah dihapus`};
+}
+
+module.exports = {
+    createKomik,
+    getAllKomiks,
+    getKomikById,
+    updateKomik,
+    deleteKomik
+};
